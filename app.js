afead6995928bf58a48f64f4592d56d45f0fdc2f
ARTALEAL <artaleal@yandex.ru>
2023-02-24 21:58:50 +0300

    backend PR13

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const routeUsers = require('./routes/users');
+const routeCards = require('./routes/cards');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log('test');
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63f8ff39ae83049155aba99d',
+  };
+
+  next();
+});
+
+app.use('/users', routeUsers);
+app.use('/cards', routeCards);
+
+app.listen(PORT);
