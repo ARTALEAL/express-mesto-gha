bb1c1bae063648bd1adbf38f5df79b359681c70e
ARTALEAL <artaleal@yandex.ru>
2023-02-24 22:35:53 +0300

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ console.log('test');
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '5d8b8592978f8bd833ca8133'
   };
 
   next();
