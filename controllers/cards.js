4076fec03ea7073cdf6475b06fd2334a9eae5dc6
ARTALEAL <artaleal@yandex.ru>
2023-03-13 23:18:19 +0300

    dev: delete card

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,21 +29,18 @@ function createCard(req, res, next) {
 
 function deleteCard(req, res, next) {
   const { cardId } = req.params;
-  const userId = req.user._id;
-  console.log(req);
-  console.log(userId);
+  const { _id } = req.user;
+
   Card
     .findById(cardId)
     .then((card) => {
       if (!card) {
         throw new NotFoundError('Карточка по указанному id не найдена');
       }
-      if (card.owner.valueOf() !== userId) {
-        console.log(userId);
-        console.log(card.owner.valueOf());
+      if (card.owner.valueOf() !== _id) {
         throw new ForbiddenError('Нельзя удалить чужую карточку');
       }
-      card
+      Card
         .findByIdAndRemove(cardId)
         .then((deletedCard) => res.send(deletedCard))
         .catch(next);
